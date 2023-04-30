5c1116922ff66c74d438cb5a29ad62b6a6e74176
xShadowSoldierx <128658278+xShadowSoldierx@users.noreply.github.com>
2023-04-30 18:11:44 +0200

    Update currencyCalculator.py

--- currencyCalculator.py
+++ currencyCalculator.py
@@ -4,6 +4,7 @@
 
 import json
 import os
+import sys
 import requests
 
 import terminal_output as tOut
@@ -26,13 +27,21 @@ def main():
     print_currencies(currencies=currencies)
     
     # Get from currency
-    get_from_currency()
+    from_currency = get_from_currency(currencies=currencies)
     
     # Get to currency
-    get_to_currency()
+    to_currency = get_to_currency(currencies=currencies)
     
     # Get amount
-    get_amount()
+    amount = get_amount(currencies=currencies, from_currency=from_currency)
+    
+    # Calculate value
+    calc_value(
+        currencies=currencies,
+        from_currency=from_currency,
+        to_currency=to_currency,
+        amount=amount
+    )
     
     # Rerun application
     rerun_app()
@@ -81,10 +90,10 @@ def fetch_data(currencies, path):
             
             with open(file_name, 'w', encoding='utf-8') as f:
                 json.dump(data, f, ensure_ascii=False, indent=4)
+                
+        tOut.success_exchange_rates_updated()
     except:
-        print('''**********************************************************************
-* No connection to the API-server. The data might not be the latest. *
-**********************************************************************''')
+        tOut.error_api_connection_failed()
 
 
 def print_currencies(currencies):
@@ -100,16 +109,67 @@ def print_currencies(currencies):
         print(f"{currency:<25} |  {short:<5}")
 
 
-def get_from_currency():
-    pass
+def get_from_currency(currencies, from_currency=None):
+    try:
+        from_currency = input('\nChoose the first currency: ').lower()
+        
+        if len(from_currency) == 3:
+            from_currency = list(currencies.keys())[list(currencies.values()).index(from_currency.upper())]
+        
+        if from_currency not in currencies:
+            raise ValueError
+    except KeyboardInterrupt:
+        tOut.error_keyboard_interrupt()
+        sys.exit()
+    except:
+        tOut.error_try_again()
+        return get_from_currency(from_currency)
+    
+    return from_currency
 
 
-def get_to_currency():
-    pass
+def get_to_currency(currencies, to_currency=None):
+    try:
+        to_currency = input('\nChoose the second currency: ').lower()
+        
+        if len(to_currency) == 3:
+            to_currency = list(currencies.keys())[list(currencies.values()).index(to_currency.upper())]
+        
+        if to_currency not in currencies:
+            raise ValueError
+    except KeyboardInterrupt:
+        tOut.error_keyboard_interrupt()
+        sys.exit()
+    except:
+        tOut.error_try_again()
+        return get_to_currency(to_currency)
+    
+    return to_currency
 
 
-def get_amount():
-    pass
+def get_amount(currencies, from_currency, amount=None):
+    try:
+        amount = float(input(f'\nEnter the amount of {currencies[from_currency]}: '))
+    except KeyboardInterrupt:
+        tOut.error_keyboard_interrupt()
+        sys.exit()
+    except:
+        tOut.error_try_again()
+        return get_amount(amount)
+    
+    return amount
+
+
+def calc_value(currencies, from_currency, to_currency, amount):
+    from_short = currencies[from_currency]
+    to_short = currencies[to_currency]
+
+    with open(f'currency/currency_{from_short}.json', 'r') as f:
+        data = json.load(f)
+
+    new_amount = round(amount * data['rates'][to_short], 2)
+
+    print(f'{amount:.2f} {from_short} are converted {new_amount:.2f} {to_short}!')
 
 
 def rerun_app():
