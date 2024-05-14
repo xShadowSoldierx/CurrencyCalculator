e9454cb77c3b64d70d123054f488f79cad3bd7fc
Ricardo Boock <r.boock.coding@proton.me>
2024-05-14 16:49:02 +0200

    Refactor functions

--- currency_calculator.py
+++ currency_calculator.py
@@ -1,9 +1,10 @@
-'''
+"""
 An application to convert one currency to another currency
 Copyright (C) Ricardo Boock - 2023
-'''
+"""
 
-
+from typing import Dict
+import concurrent.futures
 import json
 import os
 import requests
@@ -11,15 +12,22 @@ import requests
 import terminal_output as tOut
 
 
-def main():
-    FILE = os.path.abspath(__file__)
-    PATH = os.path.dirname(FILE)
+def main() -> None:
+    """
+    This function serves as the entry point of the program.
+
+    It initializes the file path, prints the logo, sets available currencies,
+    and fetches data from an API.
+    """
+
+    FILE: str = os.path.abspath(__file__)
+    PATH: str = os.path.dirname(FILE)
 
     # Welcome
     tOut.print_logo()
 
     # Set available currencies
-    currencies = set_currencies()
+    currencies: Dict[str, str] = set_currencies()
 
     # Fetch data from API and save to json files
     fetch_data(currencies=currencies, path=PATH)
@@ -28,13 +36,13 @@ def main():
     print_currencies(currencies=currencies)
 
     # Get from currency
-    from_currency = get_from_currency(currencies=currencies)
+    from_currency: str = get_from_currency(currencies=currencies)
 
     # Get to currency
-    to_currency = get_to_currency(currencies=currencies)
+    to_currency: str = get_to_currency(currencies=currencies)
 
     # Get amount
-    amount = get_amount(currencies=currencies, from_currency=from_currency)
+    amount: float = get_amount(currencies=currencies, from_currency=from_currency)
 
     # Calculate and return the converted value
     calc_value(
@@ -42,142 +50,247 @@ def main():
         currencies=currencies,
         from_currency=from_currency,
         to_currency=to_currency,
-        amount=amount
+        amount=amount,
     )
 
     # Rerun application
     rerun_app()
 
 
-def set_currencies():
-    allowed_currency = {
-        'armenischer dram': 'AMD',
-        'australischer dollar': 'AUD',
-        'brasilianischer real': 'BRL',
-        'bitcoin': 'BTC',
-        'kanadischer dollar': 'CAD',
-        'schweizer franken': 'CHF',
-        'china yuan renminbi': 'CNY',
-        'euro': 'EUR',
-        'britisches pfund': 'GBP',
-        'hongkong-dollar': 'HKD',
-        'indische rupie': 'INR',
-        'südkoreanischer won': 'KRW',
-        'mexikanischer peso': 'MXN',
-        'norwegische krone': 'NOK',
-        'neuseeland-dollar': 'NZD',
-        'russischer rubel': 'RUB',
-        'schwedische krone': 'SEK',
-        'singapur-dollar': 'SGD',
-        'türkische lira': 'TRY',
-        'us dollar': 'USD',
-        'japanischer yen': 'JPY',
-        'venezolanischer bolívar': 'VES',
-        'südafrikanischer rand': 'ZAR'
+def set_currencies() -> Dict[str, str]:
+    """
+    Sets the available currencies and returns a dictionary mapping long currency names
+    to their short codes.
+
+    Returns:
+        Dict[str, str]: A dictionary mapping long currency names to their short codes.
+    """
+
+    allowed_currency: Dict[str, str] = {
+        "armenischer dram": "AMD",
+        "australischer dollar": "AUD",
+        "brasilianischer real": "BRL",
+        "bitcoin": "BTC",
+        "kanadischer dollar": "CAD",
+        "schweizer franken": "CHF",
+        "china yuan renminbi": "CNY",
+        "euro": "EUR",
+        "britisches pfund": "GBP",
+        "hongkong-dollar": "HKD",
+        "indische rupie": "INR",
+        "südkoreanischer won": "KRW",
+        "mexikanischer peso": "MXN",
+        "norwegische krone": "NOK",
+        "neuseeland-dollar": "NZD",
+        "russischer rubel": "RUB",
+        "schwedische krone": "SEK",
+        "singapur-dollar": "SGD",
+        "türkische lira": "TRY",
+        "us dollar": "USD",
+        "japanischer yen": "JPY",
+        "venezolanischer bolívar": "VES",
+        "südafrikanischer rand": "ZAR",
     }
 
     return allowed_currency
 
 
-def fetch_data(currencies, path):
+def fetch_data(currencies: Dict[str, str], path: str) -> None:
+    """
+    Fetches data from an API based on the provided currencies and saves it to JSON files.
+
+    Parameters:
+    - currencies (Dict[str, str]): A dictionary mapping currency names to their short codes.
+    - path (str): The path where the JSON files will be saved.
+
+    Returns:
+    None
+    """
+
     try:
-        for i in currencies:
-            file = currencies[i]
+        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+            futures = {
+                executor.submit(
+                    requests.get,
+                    f"https://api.exchangerate.host/latest?base={file}",
+                    timeout=10,
+                ): file
+                for file in currencies
+            }
 
-            response = requests.get(f'https://api.exchangerate.host/latest?base={file}')
-            data = response.json()
+            for future in concurrent.futures.as_completed(futures):
+                file_name: str = f"{path}/currency/currency_{futures[future]}.json"
+                os.makedirs(os.path.dirname(file_name), exist_ok=True)
 
-            file_name = f'{path}/currency/currency_{file}.json'
-            os.makedirs(os.path.dirname(file_name), exist_ok=True)
-
-            with open(file_name, 'w', encoding='utf-8') as f:
-                json.dump(data, f, ensure_ascii=False, indent=4)
+                with open(file_name, "w", encoding="utf-8") as f:
+                    json.dump(future.result().json(), f, ensure_ascii=False, indent=4)
 
         tOut.success_exchange_rates_updated()
-    except Exception:
+    except requests.exceptions.RequestException:
         tOut.error_api_connection_failed()
 
 
-def print_currencies(currencies):
-    content = f'''Here is a list of all available currencies:
+def print_currencies(currencies: Dict[str, str]) -> None:
+    """
+    A function to print a list of all available currencies with their corresponding short codes.
+
+    Parameters:
+        currencies (Dict[str, str]): A dictionary mapping long currency names to their short codes.
+
+    Returns:
+        None
+    """
+
+    content: str = f"""Here is a list of all available currencies:
 
     {'Currency':<25} | {'Short':<5}
     ---------------------------------
-'''
+"""
     tOut.print_characters(content=content, speed=tOut.OUTPUT_SPEED)
 
     for currency, short in currencies.items():
-        if currency == 'us dollar':
-            currency = 'US Dollar'
+        if currency == "us dollar":
+            currency = "US Dollar"
         else:
             currency = currency.title()
-        tOut.print_characters(content=f"    {currency:<25} |  {short:<5}\n", speed=tOut.OUTPUT_SPEED*0.5)
+        tOut.print_characters(
+            content=f"    {currency:<25} |  {short:<5}\n", speed=tOut.OUTPUT_SPEED * 0.5
+        )
 
 
-def get_from_currency(currencies):
-    try:
-        from_currency = input('\n💶 Choose the first currency: ').lower()
+def get_from_currency(currencies: Dict[str, str]) -> str:
+    """
+    A function to get the user's input for the first currency choice.
+
+    Parameters:
+        currencies (Dict[str, str]): A dictionary mapping currency names to their short codes.
+
+    Returns:
+        str: The selected currency code.
+    """
+
+    while True:
+        from_currency = input("\n💶 Choose the first currency: ").lower()
+
+        if from_currency in currencies:
+            return from_currency
 
         if len(from_currency) == 3:
-            from_currency = list(currencies.keys())[list(currencies.values()).index(from_currency.upper())]
+            from_currency = list(currencies.keys())[
+                list(currencies.values()).index(from_currency.upper())
+            ]
 
-        if from_currency not in currencies:
-            raise Exception
-    except Exception:
+        if from_currency in currencies:
+            return from_currency
+
         tOut.error_try_again()
-        return get_from_currency(currencies)
-
-    return from_currency
 
 
-def get_to_currency(currencies):
-    try:
-        to_currency = input('\n💴 Choose the second currency: ').lower()
+def get_to_currency(currencies: Dict[str, str]) -> str:
+    """
+    A function to get the user's input for the second currency choice.
 
-        if len(to_currency) == 3:
-            to_currency = list(currencies.keys())[list(currencies.values()).index(to_currency.upper())]
+    Parameters:
+        currencies (Dict[str, str]): A dictionary mapping currency names to their short codes.
 
-        if to_currency not in currencies:
-            raise Exception
-    except Exception:
+    Returns:
+        str: The selected currency code.
+    """
+
+    to_currency: str = input("\n💴 Choose the second currency: ").lower()
+
+    if len(to_currency) == 3:
+        to_currency = list(currencies.keys())[
+            list(currencies.values()).index(to_currency.upper())
+        ]
+    elif to_currency not in currencies:
         tOut.error_try_again()
         return get_to_currency(currencies)
 
     return to_currency
 
 
-def get_amount(currencies, from_currency):
-    try:
-        amount = float(input(f'\n💰 Enter the amount of {currencies[from_currency]}: '))
-    except Exception:
-        tOut.error_try_again()
-        return get_amount(currencies, from_currency)
+def get_amount(currencies: Dict[str, str], from_currency: str) -> float:
+    """
+    A function to get the user's input for the amount of a specific currency.
 
-    return amount
+    Parameters:
+        currencies (Dict[str, str]): A dictionary mapping currency names to their short codes.
+        from_currency (str): The currency code for which the amount is being entered.
 
+    Returns:
+        float: The amount entered by the user.
+    """
 
-def calc_value(path, currencies, from_currency, to_currency, amount):
-    from_short = currencies[from_currency]
-    to_short = currencies[to_currency]
-
-    with open(f'{path}/currency/currency_{from_short}.json', 'r') as f:
-        data = json.load(f)
-
-    new_amount = round(amount * data['rates'][to_short], 2)
-
-    print(f'\n\033[1;32m💵 {amount:.2f} {from_short} are converted {new_amount:.2f} {to_short}!\033[0m\n')
-
-
-def rerun_app():
-    rerun = input("Do you want to restart the program (y/n)? ")
-    if rerun == "y":
-        main()
-    elif rerun == "n":
-        print("Exiting program...")
-    else:
-        tOut.error_try_again()
-        rerun_app()
+    while True:
+        try:
+            return float(
+                input(f"\n💰 Enter the amount of {currencies[from_currency]}: ")
+            )
+        except ValueError:
+            tOut.error_try_again()
 
 
-if __name__ == '__main__':
+def calc_value(
+    path: str,
+    currencies: Dict[str, str],
+    from_currency: str,
+    to_currency: str,
+    amount: float,
+) -> None:
+    """
+    A function to calculate the converted amount from one currency to another based on the
+    provided currency rates.
+
+    Parameters:
+        path (str): The path to the currency data files.
+        currencies (Dict[str, str]): A dictionary mapping currency names to their short codes.
+        from_currency (str): The code of the currency to convert from.
+        to_currency (str): The code of the currency to convert to.
+        amount (float): The amount of the currency to convert.
+
+    Returns:
+        None
+    """
+
+    from_short: str = currencies[from_currency]
+    to_short: str = currencies[to_currency]
+
+    with open(
+        f"{path}/currency/currency_{from_short}.json", "r", encoding="utf-8"
+    ) as f:
+        data: dict = json.load(f)["rates"]
+
+    new_amount: float = round(amount * data[to_short], 2)
+
+    print(
+        f"\n\033[1;32m💵 {amount:.2f} {from_short} are converted "
+        f"{new_amount:.2f} {to_short}!\033[0m\n"
+    )
+
+
+def rerun_app() -> None:
+    """
+    A function to rerun the application based on user input.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
+
+    while True:
+        rerun = input("Do you want to restart the program (y/n)? ")[0].lower()
+        if rerun == "y":
+            main()
+            break
+        elif rerun == "n":
+            print("Exiting program...")
+            break
+        else:
+            tOut.error_try_again()
+
+
+if __name__ == "__main__":
     main()
